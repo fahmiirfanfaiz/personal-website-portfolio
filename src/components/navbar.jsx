48c3f26efec2e1829734c7b27c7e95b1598623b6
Fahmi Irfan Faiz <fahmiirfanfaiz01@gmail.com>
2025-02-06 18:32:39 +0700

    style(navbar): update navbar styling with new fonts and text colors

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,16 +1,17 @@
 import React from "react";
+import { useState, useEffect, useRef } from 'react';
+// import './App.css';
 
 const Navbar = () => {
     return(
         <div className="w-screen flex justify-between h-[6vw] bg-[#62CBF9]">
-            <div className="h-full text-white font-bold flex justify-start items-center ml-[4vw]">
-                fahmiirfanfaiz.
+            <div className="h-full font-poppins font-bold text-[#005F73] flex justify-start items-center ml-[4vw]">
+                fahmiirfan <span className="text-[#E3F2FD]">faiz.</span> 
             </div>
-            <ul className="h-full text-white flex justify-end items-center cursor-pointer gap-x-[3vw] mr-[4vw]">
+            <ul className="h-full text-white font-montserrat flex justify-end items-center cursor-pointer gap-x-[3vw] mr-[4vw]">
                 <li>Home</li>
                 <li>About</li>
-                <li>Techstack</li>
-                <li>Education</li>
+                <li>Skills</li>
                 <li>Projects</li>
                 <li>Contact</li>
             </ul>
