418702e73b61df2c46fdb990212d12445e330e8d
Fahmi Irfan Faiz <fahmiirfanfaiz01@gmail.com>
2025-02-08 23:47:30 +0700

    feat(about): add about section prototype

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
+
 const About = () => {
     return(
-        <div>
-            About Section
+        <div className="flex flex-row justify-center w-screen gap-[5vw] mt-8"> 
+            <div>
+                Hi! I'am Fahmi Irfan Faiz
+            </div>
+            <div className="w-40 h-40"> 
+                <img src="/images/personal-profile-photo.jpeg" alt="personal-profile-photo" />
+            </div>
         </div>
     )
 }
